a66caf3cc4c544bad2c47ad20bb60ea78958796d
mohamedgamal21 <149878287+mohamedgamal21@users.noreply.github.com>
2024-10-02 16:34:11 +0300

    first commit

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -6,7 +6,7 @@ export default function Header({ onSearchChange, onSortChange }) {
     return (
         <div className="flex justify-between">
             <SearchBar onSearchChange={onSearchChange} />
-            <SortItem onSortChange={onSortChange} />]
+            <SortItem onSortChange={onSortChange} />
         </div>
     );
 }
